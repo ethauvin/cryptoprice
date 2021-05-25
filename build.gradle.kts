94193e34edddb70de2a3ca0e1c87fa12a9ad00d3
Erik C. Thauvin <erik@thauvin.net>
2021-05-25 13:58:19 -0700

    Added snapshot badge.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.17.1"
     id("org.jetbrains.dokka") version "1.4.32"
     id("org.sonarqube") version "3.2.0"
-    kotlin("jvm") version "1.5.0"
+    kotlin("jvm") version "1.5.10"
     application
     `maven-publish`
     jacoco
