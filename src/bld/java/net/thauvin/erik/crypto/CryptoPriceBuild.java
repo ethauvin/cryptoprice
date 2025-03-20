c49771796e164919a204865c51f616223458a20c
Erik C. Thauvin <erik@thauvin.net>
2025-03-20 09:13:33 -0700

    Add Kotlin compile options for JDK 24

--- src/bld/java/net/thauvin/erik/crypto/CryptoPriceBuild.java
+++ src/bld/java/net/thauvin/erik/crypto/CryptoPriceBuild.java
@@ -39,6 +39,7 @@ import rife.bld.extension.DokkaOperation;
 import rife.bld.extension.JacocoReportOperation;
 import rife.bld.extension.dokka.LoggingLevel;
 import rife.bld.extension.dokka.OutputFormat;
+import rife.bld.extension.kotlin.CompileOptions;
 import rife.bld.operations.exceptions.ExitStatusException;
 import rife.bld.publish.PomBuilder;
 import rife.bld.publish.PublishDeveloper;
@@ -120,8 +121,11 @@ public class CryptoPriceBuild extends Project {
     @BuildCommand(summary = "Compiles the Kotlin project")
     @Override
     public void compile() throws Exception {
+        final var options = new CompileOptions();
+        options.jvmOptions().add("--enable-native-access=ALL-UNNAMED");
         new CompileKotlinOperation()
                 .fromProject(this)
+                .compileOptions(options)
                 .execute();
     }
 
