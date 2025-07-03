981a9384af9be7b699389e6c964e552d4d7c17a8
Erik C. Thauvin <erik@thauvin.net>
2025-07-03 13:01:55 -0700

    Bump Kotlin to version 2.2.0

--- src/bld/java/net/thauvin/erik/crypto/CryptoPriceBuild.java
+++ src/bld/java/net/thauvin/erik/crypto/CryptoPriceBuild.java
@@ -73,7 +73,7 @@ public class CryptoPriceBuild extends Project {
         downloadSources = true;
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL);
 
-        final var kotlin = version(2, 1, 21);
+        final var kotlin = version(2, 2, 0);
         scope(compile)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin))
                 .include(dependency("org.json", "json", "20250517"))
