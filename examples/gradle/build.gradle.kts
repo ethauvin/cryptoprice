68ce8438020678f8ff08eab07077f19462e70b72
Erik C. Thauvin <erik@thauvin.net>
2025-09-12 02:18:36 -0700

    Bump Kotlin to version 2.2.20

--- examples/gradle/build.gradle.kts
+++ examples/gradle/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("application")
     id("com.github.ben-manes.versions") version "0.51.0"
-    kotlin("jvm") version "2.2.0"
+    kotlin("jvm") version "2.2.20"
 }
 
 defaultTasks(ApplicationPlugin.TASK_RUN_NAME)
