94cc24caef4e6cb8e0fe69fb9069aaa7021bfeaa
Erik C. Thauvin <erik@thauvin.net>
2025-11-01 23:00:31 -0700

    Bump JUnit to version 6.0.1

--- src/bld/java/net/thauvin/erik/crypto/CryptoPriceBuild.java
+++ src/bld/java/net/thauvin/erik/crypto/CryptoPriceBuild.java
@@ -73,7 +73,7 @@ public class CryptoPriceBuild extends Project {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_SNAPSHOTS, RIFE2_RELEASES);
 
         final var kotlin = version(2, 2, 21);
-        final var junit = version(6, 0, 0);
+        final var junit = version(6, 0, 1);
         scope(compile)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin))
                 .include(dependency("org.json", "json", "20250517"))
