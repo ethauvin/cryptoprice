b71c8d6a0a907f4d7be412c15c5399a453f6107a
Erik C. Thauvin <erik@thauvin.net>
2026-01-27 07:00:50 -0800

    Use IOTools to resolve test results directory

--- src/bld/java/net/thauvin/erik/crypto/CryptoPriceBuild.java
+++ src/bld/java/net/thauvin/erik/crypto/CryptoPriceBuild.java
@@ -36,7 +36,7 @@ import rife.bld.Project;
 import rife.bld.extension.*;
 import rife.bld.extension.dokka.LoggingLevel;
 import rife.bld.extension.dokka.OutputFormat;
-import rife.bld.extension.tools.IOUtils;
+import rife.bld.extension.tools.IOTools;
 import rife.bld.operations.exceptions.ExitStatusException;
 import rife.bld.publish.PomBuilder;
 import rife.bld.publish.PublishDeveloper;
@@ -57,7 +57,7 @@ import static rife.bld.dependencies.Scope.*;
 public class CryptoPriceBuild extends Project {
 
     final File srcMainKotlin = new File(srcMainDirectory(), "kotlin");
-    final File testResultsDirectory = IOUtils.resolveFile(buildDirectory(), "test-results", "test");
+    final File testResultsDirectory = IOTools.resolveFile(buildDirectory(), "test-results", "test");
 
     public CryptoPriceBuild() {
         pkg = "net.thauvin.erik.crypto";
